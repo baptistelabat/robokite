50992031229ea903418935613cd5e1e561b04c91
Baptiste LABAT <Baptiste.LABAT@gmail.com>
2013-06-16 23:19:44 +0200

    Correct typing error and arrange indentation

--- Control/PID.py
+++ Control/PID.py
@@ -1,11 +1,11 @@
 class PID:
 	def __init__(self, Kp=1, Ki=0.1, Kd=1, maxIntegralCorrection=0, minIntegralCorrection=-0):
-	  self.Kp = Kp # Proporiional gain
+	  self.Kp = Kp # Proportional gain
 	  self.Ki = Ki # Integral gain
 	  self.Kd = Kd # Derivative gain
 	  self.integral = 0
 	def incrementTime(self, error, dt):
 	  self.integral = self.integral + error*self.Ki*dt
-        def computeCorrection(self, error, derror):
-          correction = self.Kp*(error) + self.Kd*derror + self.integral
-          return correction
+    def computeCorrection(self, error, derror):
+       correction = self.Kp*(error) + self.Kd*derror + self.integral
+       return correction

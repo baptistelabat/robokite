b3280f3d7e69c232772b8c8709cb57eb687e2b63
Baptiste Labat <baptiste.labat@gmail.com>
2013-07-26 20:26:06 +0200

    Add proof of concept of the use of Web socket -to react to modification by user -to display information Add sliders

--- ManMachineInterface/script.js
+++ ManMachineInterface/script.js
@@ -1,19 +1,53 @@
+function DispatchText(){
+                var userInput = document.getElementById("setOrientationRange").value;
+                ws.send(userInput);
+}
 function setManualControl(){
 var myOutput = document.getElementById("controlMode");
 myOutput.value = "Manual control";
 } // end change
+
 function setJoystickControl(){
 var myOutput = document.getElementById("controlMode");
 myOutput.value = "Joystick control";
 } // end change
+
 function setAutomaticControl(){
 var myOutput = document.getElementById("controlMode");
 myOutput.value = "Automatic control";
 } // end change
-function updateOutput(){
+
+function updateSetOrientation(){
 //get elements
-var myRange = document.getElementById("myRange");
-var myOutput = document.getElementById("myOutput2");
+var myRange = document.getElementById("setOrientationRange");
+var myOutput = document.getElementById("setOrientation");
+//copy the value over
+myOutput.value = myRange.value;
+DispatchText()
+} // end function
+
+function updateKp(){
+//get elements
+var myRange = document.getElementById("KpRange");
+var myOutput = document.getElementById("Kp");
 //copy the value over
 myOutput.value = myRange.value;
 } // end function
+
+function updateKi(){
+//get elements
+var myRange = document.getElementById("KiRange");
+var myOutput = document.getElementById("Ki");
+//copy the value over
+myOutput.value = myRange.value;
+} // end function
+
+function updateKd(){
+//get elements
+var myRange = document.getElementById("KdRange");
+var myOutput = document.getElementById("Kd");
+//copy the value over
+myOutput.value = myRange.value;
+} // end function
+
+

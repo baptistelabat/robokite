8930cc6d4f53f84e12c66c28dc970c287791d89d
Baptiste Labat <baptiste.labat@gmail.com>
2013-08-07 10:09:58 +0200

    Move script. js according to Tornado example architecture Add control of motor through web interface

--- ManMachineInterface/server.py
+++ ManMachineInterface/server.py
@@ -5,7 +5,9 @@ import tornado.web
 import tornado.websocket
 import tornado.ioloop
 import datetime
-
+import os
+import serial
+import numpy as np
 clients = []
  
 class MainHandler(tornado.web.RequestHandler):
@@ -15,21 +17,34 @@ class MainHandler(tornado.web.RequestHandler):
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
         self.write_message(u"Status OK " + message)
+        global ser
+	ser.write(str(np.floor((float(message)-128)/128.*100)/100.0))
+        print str(np.floor((float(message)-128)/128.)/100.0)
+
     def open(self):
+ 	global ser
+	ser = serial.Serial('/dev/ttyACM0', 9600)
+	ser.write('i') # i to start serial control
         clients.append(self)
 	self.write_message(u"Connected")
+        print "open"
     def close(self):
 	self.write_message(u"Connection closed")
  	clients.remove(self)
+        print "close"
 
     def test(self):
         self.write_message(u"2")
         print "2"
- 
-application = tornado.web.Application([
+
+handlers = [
     (r"/", MainHandler),
     (r"/websocket", WebSocketHandler),
-])
+]
+settings = dict(
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+)
+application = tornado.web.Application(handlers, **settings)
 
 def timer():
     for c in clients:
@@ -37,10 +52,10 @@ def timer():
 
  
 if __name__ == "__main__":
-    application.listen(8888)
+    application.listen(8080)
     mainLoop = tornado.ioloop.IOLoop.instance()
-    scheduler = tornado.ioloop.PeriodicCallback(timer, 1000, io_loop = mainLoop)
-    scheduler.start()
+    #scheduler = tornado.ioloop.PeriodicCallback(timer, 1000, io_loop = mainLoop)
+    #scheduler.start()
     mainLoop.start()
     
 

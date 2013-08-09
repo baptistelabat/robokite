5feb53533135bc28d31918cecb9673629ef66b0b
Baptiste Labat <baptiste.labat@gmail.com>
2013-08-09 20:38:45 +0200

    try different serial port when opening connection Sent only valid command to avoid rejection.

--- Control/controlFromMobile.py
+++ Control/controlFromMobile.py
@@ -7,15 +7,35 @@ sys.path.append(os.getcwd())
 sys.path.append('/media/bat/DATA/Baptiste/Nautilab/kite_project/robokite/ObjectTracking')
 import mobileState
 import serial
+
+# Get the mobile orientation
 mobile = mobileState.mobileState()
 a = threading.Thread(None, mobileState.mobileState.checkUpdate, None, (mobile,))
 a.start()
-ser = serial.Serial('/dev/ttyACM1', 9600)
-dt = 0.1
-time.sleep(dt)
+
+# Open the serial port
+locations=['/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2','/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3','/dev/ttyS0','/dev/ttyS1','/dev/ttyS2','/dev/ttyS3']
+for device in locations:
+  try:
+    print "Trying...",device
+    ser = serial.Serial(
+	port = device,
+	baudrate = 9600
+	)
+    break
+  except:
+    print "Failed to connect on",device
+time.sleep(1.5) # Arduino is reset when opening port so wait before communicating
+ser.flush()
 ser.write('i1')
+dt = 0.1
 while True:
     mobile.computeRPY()
-    ser.write(str(np.floor(100*mobile.roll/3)/100.0))
+    ser.write(
+	      str(	
+                   np.max([-0.99, np.min([0.99, np.floor(100*mobile.roll/2.0)/100.0])]
+                          )
+	          )
+        )
     print mobile.roll
     time.sleep(dt)

282fd361f9bb3bc6baf27306994f70b3868a6cde
Baptiste Labat <baptiste.labat@gmail.com>
2013-10-19 20:42:40 +0200

    Hide the scene and load the kite model

--- Simulator/panda.py
+++ Simulator/panda.py
@@ -16,11 +16,16 @@ class MyApp(ShowBase):
         # Load the environment model.
         self.environ = self.loader.loadModel("models/environment")
         # Reparent the model to render.
-        self.environ.reparentTo(self.render)
+        #self.environ.reparentTo(self.render)
         # Apply scale and position transforms on the model.
         self.environ.setScale(0.25, 0.25, 0.25)
         self.environ.setPos(-8, 42, 0)
- 
+        
+        # Load the kite model
+        self.model=self.loader.loadModel("/media/bat/DATA/Nautilabs/kite_project/robokite/Simulator/skpfile.egg")
+        # Reparent the model to render.
+        #self.model.reparentTo(self.render)
+        
         # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
  

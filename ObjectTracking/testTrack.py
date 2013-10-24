416575ca3cc684925be0391b43b98a9fa1d9f909
Baptiste Labat <baptiste.labat@gmail.com>
2013-10-24 19:24:41 +0200

    Save the image of the selection (to be able to reinitialise later)

--- ObjectTracking/testTrack.py
+++ ObjectTracking/testTrack.py
@@ -1,5 +1,5 @@
 
-from SimpleCV import ColorSegmentation, Image, Camera, VirtualCamera, Display
+from SimpleCV import ColorSegmentation, Image, Camera, VirtualCamera, Display, Color
 
 # Open reference video
 cam=VirtualCamera('/media/bat/DATA/Baptiste/Nautilab/kite_project/zenith-wind-power-read-only/KiteControl-Qt/videos/kiteFlying.avi','video')
@@ -15,13 +15,16 @@ while (disp.isNotDone()):
         img = cam.getImage()
 	bb = (255, 180, 70, 20)
         ts = img.track("camshift",ts,modelImage,bb, num_frames = 1)
+	modelImage = Image('kite_detail.jpg')
         # now here in first loop iteration since ts is empty,
         # img0 and bb will be considered.
         # New tracking object will be created and added in ts (TrackSet)
         # After first iteration, ts is not empty and hence the previous
         # image frames and bounding box will be taken from ts and img0
         # and bb will be ignored.
-	ts.drawPath()
+	ts.draw()
+	ts.drawBB()
+	ts.showCoordinates()
 	img.show()
 
 

fe6e6cd345d521e5ca5ebd94411186ee47c61166
Baptiste Labat <baptiste.labat@gmail.com>
2013-10-24 16:48:48 +0200

    Reorder code

--- ManMachineInterface/server.py
+++ ManMachineInterface/server.py
@@ -20,7 +20,16 @@ import numpy as np
 import time
 sys.path.append('..')
 
-global ser
+global ser # Serial communication
+global alpha # Pulse Width Modulation between -1 and 1
+alpha = 0
+global serialPending
+serialPending = ''
+global serialHistory
+serialHistory = ''
+mostRecentLine = ''
+clients = [] # Stores the web client which are connected.
+
 
 
 def computeXORChecksum(chksumdata):
@@ -36,31 +45,46 @@ def computeXORChecksum(chksumdata):
         # and stores the new XOR value in csum
         csum ^= ord(c)
     h = hex(csum)    
-    return h[2:]#get hex data without 0x prefix
+    return h[2:] # Get hex data without 0x prefix
     
-clients = []
-global alpha
-alpha = 0
-
-# gets serial input in a non-blocking way
+def openSerial():
+  global ser
+  
+  # Loop over varying serial port till you find one (assume you have only one device connected)
+  locations = ['/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2','/dev/ttyACM3','/dev/ttyACM4','/dev/ttyACM5','/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3','/dev/ttyS0','/dev/ttyS1','/dev/ttyS2','/dev/ttyS3']
+  for device in locations:
+    try:
+      print "Trying...",device
+      ser = serial.Serial(device, baudrate=19200, timeout=1)
+      print "Connected on ", device
+      break
+    except:
+      print "Failed to connect on ", device
+      
+  time.sleep(1.5) # Arduino is reset when opening port so wait before communicating
+  # An alternative would be to listen to a message from the arduino saying it is ready
+  ser.write('i1') # i to start serial control, 1 is the minimum expecting message frequency (in house protocol)
 
 def updateSerial():
-	global alpha
-	global ser
-	try:
-		msg = "ORPWM"+","+str(alpha)
-		msg = "$"+msg +"*"+ computeXORChecksum(msg) + chr(13).encode('ascii')
-		print "send " + msg
+    global alpha
+    global ser
+	
+    try:
+		# Send an in house proprietary message
+		# 0: stands for open-source (by contradiction to P, which stands for proprietary!)
+		# R: stands for robokite, the name of the project
+		# PWM: stands for Pulse Width Modulation, the way the motor is controlled.
+		msg = "ORPWM" + "," + str(alpha)
+		msg = "$" + msg + "*" + computeXORChecksum(msg) + chr(13).encode('ascii')
+		print "Send " + msg
 		ser.write(msg)
+    except Exception, e:
+        print("Serial exception: " + str(e))
 
-	except Exception, e:
-		print("Serial exception" + str(e))
-
-global serialPending
-serialPending = ''
 def checkSerial():
     global ser
     global serialPending
+    
     try:
         s = ser.readline()
         print "Received from arduino: " + s
@@ -74,15 +98,13 @@ def checkSerial():
     
 
 #called whenever there is new input to check
-global serialHistory
-serialHistory = ''
-mostRecentLine = ''
+
 def parseSerial():
     global serialHistory
+    
     split = serialPending.split("\r\n")
     if len( split ) > 1:
         for line in split[0:-1]:
-            #print( line )
             
             #do some stuff with the line, if necessary
             #example:
@@ -92,22 +114,6 @@ def parseSerial():
             #serialHistory += line
 
         pending = split[-1]
-       
-def openSerial():
-  global ser
-  locations=['/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2','/dev/ttyACM3','/dev/ttyACM4','/dev/ttyACM5','/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3','/dev/ttyS0','/dev/ttyS1','/dev/ttyS2','/dev/ttyS3']
-  for device in locations:
-    try:
-      print "Trying...",device
-      ser = serial.Serial(device, baudrate=19200, timeout=1)
-      print "Connected on ", device
-      break
-    except:
-      print "Failed to connect on ", device
-  time.sleep(1.5) # Arduino is reset when opening port so wait before communicating
-  # An alternative would be to listen to a message from the arduino saying it is ready
-  ser.write('i1') # i to start serial control
-
  
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -115,10 +121,12 @@ class MainHandler(tornado.web.RequestHandler):
  
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
-        self.write_message(u"Status OK " + message)
-        print "received message from MMI: " + message
         global ser
         global alpha
+        
+        self.write_message(u"Status OK " + message)
+        print "received message from MMI: " + message
+
         try:
           alpha = np.floor((float(message)-128)/128.*100)/100.0
           msg = "ORPWM" + "," + str(alpha)
@@ -128,29 +136,25 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         except:
           print "time out exception"
         
-
     def open(self):
       global alpha
+      
       openSerial()
       alpha = 0
       clients.append(self)
       self.write_message(u"Connected")
-      print "open"
+      print "Open web client connection"
       
     def on_close(self):
       clients.remove(self)
-      print "close"
-
-    def test(self):
-        self.write_message(u"2")
-        print "2"
+      print "Close web client connection"
 
 handlers = [
     (r"/", MainHandler),
     (r"/websocket", WebSocketHandler),
 ]
 settings = dict(
-            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            static_path = os.path.join(os.path.dirname(__file__), "static"),
 )
 application = tornado.web.Application(handlers, **settings)
 
@@ -158,7 +162,6 @@ def timer():
     for c in clients:
         c.write_message(datetime.datetime.utcnow().strftime("%Y%m%d_%Hh%Mm_%Ss"))
 
- 
 if __name__ == "__main__":
     openSerial()
     application.listen(8080)

ed37275f56d540e929350db9cdb39cd2951b8006
Baptiste Labat <baptiste.labat@gmail.com>
2013-11-10 20:59:00 +0100

    Adding oscilloscope (real time chart) into browser demo

---
+++ ManMachineInterface/Oscilloscope/server.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+# Copyright (c) 2013 Baptiste LABAT
+#
+# Licensed under the MIT License,
+# https://github.com/baptistelabat/robokite
+# Authors: Baptiste LABAT
+#
+# Used http://www.linuxforu.com/2012/04/getting-started-with-html5-websockets/
+ 
+import tornado.web
+import tornado.websocket
+import tornado.ioloop
+import os
+import datetime
+import time
+import json
+import math
+clients = []
+global t0
+t0 = time.time()
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("graph.html")
+ 
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def on_message(self, message):  
+      print "message received"     
+
+    def open(self):
+      self.vote = 50
+      clients.append(self)
+      self.write_message(u"Connected")
+      print "open"
+      
+    def on_close(self):
+      clients.remove(self)
+      print "close"
+
+handlers = [
+    (r"/", MainHandler),
+    (r"/websocket", WebSocketHandler),
+]
+settings = dict(
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+)
+application = tornado.web.Application(handlers, **settings)
+
+def timer():
+    global t0
+    for c in clients:
+        #c.write_message(datetime.datetime.utcnow().strftime("%Y%m%d_%Hh%Mm_%Ss"))
+        t = time.time()-t0
+        c.write_message( json.dumps({'x':t, 'y':500+500*math.sin(t)})
+)
+ 
+if __name__ == "__main__":
+    application.listen(8080)
+    mainLoop = tornado.ioloop.IOLoop.instance()
+    scheduler = tornado.ioloop.PeriodicCallback(timer, 100, io_loop = mainLoop)
+    scheduler.start()
+    mainLoop.start()
+    
+

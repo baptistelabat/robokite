db1a420156435346150b042aca9584a32038b205
Baptiste Labat <baptiste.labat@gmail.com>
2013-11-15 19:37:57 +0100

    Adding comments

--- Communication/crc32.py
+++ Communication/crc32.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+# Copyright (c) 2013 Nautilabs
+#
+# Licensed under the MIT License,
+# https://github.com/baptistelabat/robokite
+# Authors: Baptiste LABAT, 
+#
+# This file is an example of the use of the Cyclic redundancy check
 import serial
 import time
 import binascii
-""" This file is an example of the use of the Cyclic redundancy check
-"""
 
 def openSerial():
 	global ser
@@ -19,6 +27,13 @@ def openSerial():
             time.sleep(1.5) # Arduino is reset when opening port so wait before communicating
 
 def add_crc32(msg):
+''' This function adds a crc32 key to a message before sending it over a connection with loss
+    It can then be used to check the validity of the message
+Input:
+  msg: string
+Return:
+  modified string'''
+  
   msg_ascii = msg
   crc_hex = hex(binascii.crc32(msg_ascii) & 0xffffffff)
   crc_ascii = binascii.unhexlify(crc_hex[2:])

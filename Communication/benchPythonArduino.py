18637440c296228a875517d84bb64ed3fc90dd80
Baptiste Labat <baptiste.labat@gmail.com>
2013-11-15 19:20:48 +0100

    Add comments Add default behaviour Improve behaviour in case of failure

--- Communication/benchPythonArduino.py
+++ Communication/benchPythonArduino.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+# Copyright (c) 2013 Nautilabs
+#
+# Licensed under the MIT License,
+# https://github.com/baptistelabat/robokite
+# Authors: Baptiste LABAT, 
+#
+# This file is a benchmark of the different python libraries enabling high level communication with arduino.
+# The following libraries were tested:
+#
+#   firmata                       https://github.com/lupeke/python-firmata/
+#   pyfirmata                     https://github.com/tino/pyFirmata
+#   Python-Arduino-Proto-API-v2   https://github.com/vascop/Python-Arduino-Proto-API-v2
+#   Python-Arduino-Command-API    https://github.com/thearn/Python-Arduino-Command-API 
+#   
+#   The benchmark is only based on the capability (frequency) to read analog signal from Arduino
+
 import time
 import os
 import sys
@@ -20,30 +39,37 @@ def guess_port():
             break
     return "/dev/%s" % port
 
-firmata = 0 # https://github.com/lupeke/python-firmata/
-pyfirmata = 1 # https://github.com/tino/pyFirmata
-protoApiV2 = 2 # https://github.com/vascop/Python-Arduino-Proto-API-v2
+# The library to be tested is given as argument
+if len(sys.argv) > 1:
+  nlib = int(sys.argv[1])
+else:
+  nlib = 1
+print nlib
+# The choice are among (need to be installed)
+firmata = 0                 # https://github.com/lupeke/python-firmata/
+pyfirmata = 1               # https://github.com/tino/pyFirmata
+protoApiV2 = 2              # https://github.com/vascop/Python-Arduino-Proto-API-v2
 pythonArduinoCommandAPI = 3 # https://github.com/thearn/Python-Arduino-Command-API 
 
+# Only one pin is used for the benchmark
 pin = 0
-nlib = int(sys.argv[1])
-print nlib
+
 # Load library to be tested
 if nlib == firmata:
   from firmata import Arduino, OUTPUT
 elif nlib == pyfirmata:
   from pyfirmata import util, Arduino
 elif nlib == protoApiV2:
-  sys.path.append('/home/bat/Python-Arduino-Proto-API-v2/arduino')
+  sys.path.append('/home/bat/Python-Arduino-Proto-API-v2/arduino') # There is no installation script, so this is hard coded
   from arduino import Arduino
 elif nlib == pythonArduinoCommandAPI:
   from Arduino import Arduino
 
-
 # Open serial connection
-device = guess_port()	
+device = guess_port()	# Worked so far. Replace with port if special case
+board = None
 try:
-      print "Trying...", device
+      print "Trying connection on...", device
       if (nlib==firmata) or (nlib==pyfirmata) or (nlib==protoApiV2):
         board = Arduino(device)
         print "Connected on ", device
@@ -54,45 +80,45 @@ try:
         #break
 except:
       print "Failed to connect on ", device
- 
- 
-# Initialize pin
-print "Initialising pin"
-if nlib == firmata:
-  board.pin_mode(pin, OUTPUT)
-elif nlib == pyfirmata:
-    it= util.Iterator(board)
-    it.start()
-    board.analog[pin].enable_reporting()
-elif nlib == protoApiV2:
-  board.output([])
-elif pythonArduinoCommandAPI:
-    board.pinMode(pin, "INPUT")
-  
-#  Loop and read data
-t0 = time.time()
-n = 0
-t = time.time()
-print "Looping over data"
-while t-t0<10:  
+      
+if board:
+  # Initialize pin
+  print "Initialising pin"
   if nlib == firmata:
-    board.parse()
-    value = board.analog_read(pin)
+    board.pin_mode(pin, OUTPUT)
   elif nlib == pyfirmata:
-    value = board.analog[pin].read()
+      it= util.Iterator(board)
+      it.start()
+      board.analog[pin].enable_reporting()
   elif nlib == protoApiV2:
-    value = board.analogRead(pin) # Reading from analog pin #0
-  elif nlib == pythonArduinoCommandAPI:
-    value = board.analogRead(pin)
-  n = n+1
-  print "n: ",n, value
+    board.output([])
+  elif pythonArduinoCommandAPI:
+      board.pinMode(pin, "INPUT")
+    
+  #  Loop and read data
+  t0 = time.time()
+  n = 0
   t = time.time()
- 
-# Closing connection properly
-if nlib == firmata:
-  print "No close"
-elif nlib == pyfirmata:
-  board.exit()
-elif nlib == protoApiV2:
-  board.close()
+  print "Looping over data"
+  while t-t0<10:  
+    if nlib == firmata:
+      board.parse()
+      value = board.analog_read(pin)
+    elif nlib == pyfirmata:
+      value = board.analog[pin].read()
+    elif nlib == protoApiV2:
+      value = board.analogRead(pin) # Reading from analog pin #0
+    elif nlib == pythonArduinoCommandAPI:
+      value = board.analogRead(pin)
+    n = n+1
+    print "n: ",n, value
+    t = time.time()
+   
+  # Closing connection properly
+  if nlib == firmata:
+    print "No close"
+  elif nlib == pyfirmata:
+    board.exit()
+  elif nlib == protoApiV2:
+    board.close()
 

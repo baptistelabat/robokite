6e88b85715ab1de05c527cfc7ba0f2b1ddc94481
Baptiste Labat <baptiste.labat@gmail.com>
2014-05-29 19:22:54 +0200

    Add the previous mobile phone demo

--- ManMachineInterface/3D/webGLDemo/serverMobile.py
+++ ManMachineInterface/3D/webGLDemo/serverMobile.py
@@ -36,7 +36,7 @@ except KeyboardInterrupt:
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("webgl_loader_collada.html")
+        self.render("webgl_mobile.html")
  
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):  
@@ -45,7 +45,7 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
       self.vote = 50
       clients.append(self)
-      self.write_message(u"Connected")
+      #self.write_message(u"Connected")
       print "open"
       
     def on_close(self):
@@ -70,13 +70,14 @@ def timer():
         #xrotation is through the screen by default, positive anticlockwise
         #yrotation is up by default, positive anticlockwise
         #z-axis is to the right
-        c.write_message( json.dumps({'x':0, 'y':0, 'z':0,\
-         'xrotation':mobile.roll, 'yrotation': mobile.pitch, 'zrotation':mobile.yaw,\
+        msg =  json.dumps({'x':0, 'y':0, 'z':0,\
+         'xrotation':mobile.roll, 'yrotation':mobile.pitch, 'zrotation':mobile.yaw,\
           'q0':mobile.q[0], 'q1':mobile.q[1], 'q2':mobile.q[2], 'q3':mobile.q[3],\
-           'accx': mobile.acceleration_filtered[0], 'accy': mobile.acceleration_filtered[1], 'accz': mobile.acceleration_filtered[2],\
-           'magx': mobile.magnetic_filtered[0], 'magy': mobile.magnetic_filtered[1], 'magz': mobile.magnetic_filtered[2]\
+           'accx':mobile.acceleration_filtered[0], 'accy':mobile.acceleration_filtered[1], 'accz':mobile.acceleration_filtered[2],\
+           'magx':mobile.magnetic_filtered[0], 'magy':mobile.magnetic_filtered[1], 'magz':mobile.magnetic_filtered[2]\
            })
-)
+        print msg
+        c.write_message(msg)
  
 if __name__ == "__main__":
     try:

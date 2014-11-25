21219c2ba2e6dbd82c8c9d226de96dafb5c9bb4e
Baptiste Labat <baptiste.labat@gmail.com>
2014-11-25 12:29:13 +0100

    Add a script to test mavlink connection (read heartbeat)

---
+++ Communication/mavtester.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+'''
+test mavlink messages
+Do not forget to precise the baudrate (default 115200)
+'''
+
+import sys, struct, time, os
+from curses import ascii
+
+from pymavlink import mavutil
+
+from argparse import ArgumentParser
+parser = ArgumentParser(description=__doc__)
+
+parser.add_argument("--baudrate", type=int,
+                  help="master port baud rate", default=115200)
+parser.add_argument("--device", required=True, help="serial device")
+parser.add_argument("--source-system", dest='SOURCE_SYSTEM', type=int,
+                  default=255, help='MAVLink source system for this GCS')
+args = parser.parse_args()
+
+def wait_heartbeat(m):
+    '''wait for a heartbeat so we know the target system IDs'''
+    print("Waiting for APM heartbeat")
+    msg = m.recv_match(type='HEARTBEAT', blocking=True)
+    print("Heartbeat from APM (system %u component %u)" % (m.target_system, m.target_component))
+
+# create a mavlink serial instance
+master = mavutil.mavlink_connection(args.device, baud=args.baudrate, source_system=args.SOURCE_SYSTEM)
+
+# wait for the heartbeat msg to find the system ID
+while True:
+  wait_heartbeat(master)

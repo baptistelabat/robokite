428e9711990d4cfef1be38758b68a691c7d68dd5
baptistelabat <baptiste.labat@gmail.com>
2014-12-22 11:10:58 +0000

    Add a script for automatic startup on raspberry

---
+++ robokite.sh
@@ -0,0 +1,5 @@
+# This script is for automatic launch
+# sudo mv robokite.sh /etc/init.d
+# chmod 755 /etc/init.d/robokite.sh
+# sudo update-rc.d robokite.sh defaults
+python /home/pi/robokite/Motors/motorJoy.py

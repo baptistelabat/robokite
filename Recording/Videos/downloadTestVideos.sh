7f6f5bb89c21f2d4f1d101e7c7d647ced5ba79c9
baptistelabat <baptiste.labat@gmail.com>
2015-01-28 20:57:58 +0100

    Add more videos (which were already used in some tests)

--- Recording/Videos/downloadTestVideos.sh
+++ Recording/Videos/downloadTestVideos.sh
@@ -2,4 +2,6 @@ youtube-dl https://www.youtube.com/watch?v=YTMgX1bvrTo
 youtube-dl https://www.youtube.com/watch?v=LgvpmMt-SA0
 youtube-dl https://www.youtube.com/watch?v=XAJl9fTDc7Q
 youtube-dl https://www.youtube.com/watch?v=wXHFUfO7sG8
+youtube-dl https://www.youtube.com/watch?v=jn9RrUCiWKM
+youtube-dl https://www.youtube.com/watch?v=fNMO3kAX0PE
 

ac729c5663ff3dbc75061f9c3d63e8ba09a01457
baptistelabat <baptiste.labat@gmail.com>
2015-01-28 16:50:41 +0100

    Adding other videos from Adrien Emery

--- Recording/Videos/downloadTestVideos.sh
+++ Recording/Videos/downloadTestVideos.sh
@@ -1,3 +1,5 @@
 youtube-dl https://www.youtube.com/watch?v=YTMgX1bvrTo
 youtube-dl https://www.youtube.com/watch?v=LgvpmMt-SA0
+youtube-dl https://www.youtube.com/watch?v=XAJl9fTDc7Q
+youtube-dl https://www.youtube.com/watch?v=wXHFUfO7sG8
 

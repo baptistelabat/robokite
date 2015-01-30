ad257e7730d03df984124493814656930bcb0b5f
baptistelabat <baptiste.labat@gmail.com>
2015-01-30 22:03:39 +0100

    Change port to fit with other script of tcp communication test

--- Communication/tcpServer.py
+++ Communication/tcpServer.py
@@ -4,7 +4,7 @@ import socket
 
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 54321
+TCP_PORT = 1234
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)

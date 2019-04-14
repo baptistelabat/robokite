687d7dd560c87624776c33d62b4646b6824bf3c4
Baptiste Labat <baptiste.labat@gmail.com>
2019-04-14 11:48:23 +0200

    Use updated mavlink library v2

--- Arduino/libraries/MavlinkForArduino/MavlinkForArduino.h
+++ Arduino/libraries/MavlinkForArduino/MavlinkForArduino.h
@@ -1,5 +1,5 @@
 //Move this directory to ~/sketchbook/libraries
-// and clone https://github.com/mavlink/c_library.git inside
-// git clone https://github.com/mavlink/c_library.git ~/sketchbook/libraries/MavlinkForArduino
+// and clone https://github.com/mavlink/c_library_v2.git inside
+// git clone https://github.com/mavlink/c_library_V2.git ~/Arduino/libraries/MavlinkForArduino/c_library_v2
 
-#include "c_library/ardupilotmega/mavlink.h"
+#include "c_library_v2/ardupilotmega/mavlink.h"

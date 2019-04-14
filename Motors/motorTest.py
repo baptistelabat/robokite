083a7b15387d40c4081e313a66842da0e11e010c
Baptiste Labat <baptiste.labat@gmail.com>
2019-04-14 09:44:53 +0200

    Update motor test based on modifications already done in motorJoy.py

--- Motors/motorTest.py
+++ Motors/motorTest.py
@@ -35,35 +35,54 @@ msg2 = NMEA("PW2", 0.00, "OR")
 mfb = NMEA("FBR", 0, "OR")
 dt = 0.01
 locations=['/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2','/dev/ttyACM3','/dev/ttyACM4','/dev/ttyACM5','/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3','/dev/ttyS0','/dev/ttyS1','/dev/ttyS2','/dev/ttyS3']
+ORDER_SAMPLE_TIME = 0.5 #seconds Sample time to send order without overwhelming arduino 
+FEEDBACK_SAMPLE_TIME = 0.5
 for device in locations:
   try:
     print("Trying...", device)
-    ser = serial.Serial(device, baudrate=19200, timeout=1)
+    ser = serial.Serial(device, baudrate=57600, timeout=1)
     print("Connected on ", device)
     break
   except:
     print("Failed to connect on ", device)
 time.sleep(1.5)
 t0 = time.time()
+previousTime = t0
+last_feedback_time = 0
 n = 0
 while True:
-  n = n+1
-  t = time.time()-t0
-  print("n= ", n, ", t= ", t)
-  order = 0.2*np.sin(t)
-  alpha = np.round(order, 2)
-  msg1 = NMEA("PW1", alpha, "OR")
-  msg2 = NMEA("PW2", alpha, "OR")
-  print(msg1)
-  ser.write(msg1.encode())
-  ser.write(msg2.encode())
-  print("Message sent")
-  try: #The ressource can be temporarily unavailable
-    ser.write(mfb.encode())
-    line = ser.readline()
-    print("Received from arduino: ", line)
+
+  if (time.time()-previousTime > ORDER_SAMPLE_TIME):
+    previousTime = time.time()
+    n = n+1
+    t = time.time()-t0
+    print("n= ", n, ", t= ", t)
+    order = 0.4*np.sin(t)
+    alpha = np.round(order, 2)
+    msg1 = NMEA("PW1", int(alpha*127), "OR")
+    msg2 = NMEA("PW2", int(alpha*127), "OR")
+    print(msg1)
+    ser.write(msg1.encode())
+    ser.write(msg2.encode())
+    print("Message sent")
+
+  if (time.time()-last_feedback_time > FEEDBACK_SAMPLE_TIME) :
+    last_feedback_time = time.time()
+    try:
+      ser.write(mfb.encode())
+      print("Feedback requested")
+    except Exception as e:
+      print("Error sending order: " + str(e))
+      #ser.close()
+      break
+
+  try: # The ressource can be temporarily unavailable
+    if ser.inWaiting() > 0:
+      line = ser.readline()
+      #print("Received from arduino: ", line)
+      fdbk = line.split(',')
+      print(fdbk)
   except Exception as e:
-    print("Error reading from serial port" + str(e))
-      
-  time.sleep(dt)
+    #ser.close()
+    print("Error reading from serial port: " + str(e))
 ser.close()
